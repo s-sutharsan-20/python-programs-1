96fc970658c4c5d348f87cd7c30c371518593cf6
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-05-26 22:18:05 +0530

    Add files via upload

---
+++ possitive or negative.py
@@ -0,0 +1,8 @@
+
+n=int(input("Enter any integer"))
+if (n>0):
+    print("the given number is possitive")
+elif (n<0):
+    print("The given number is negative")
+else:
+    print("The given number is zero")
