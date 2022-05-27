d1014e9a19512e9d6902a9de77546c5a8e36dbf0
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-05-27 16:14:27 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+#area of triangle
+
+height=float(input("Enter the height of the triangle :"))
+base=float(input("Enter the base of the triangle :"))
+area_of_triangle=0.5*base*height
+print("The area of the Triangle is :",area_of_triangle,"square units")
