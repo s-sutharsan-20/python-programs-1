b90a8295682769075aa52207012847704e7acb93
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-05-28 21:18:03 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+s=input("Enter any string :")
+r=s[::-1]
+print("original string :",s)
+print("Reversed string :",r)
+if(s==r):
+    print("The given string is palindrome")
+else:
+    print("The given string is not a palindrome")
