8fedbe518520e9046a86d4c991b7ae56886ea459
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-05-28 21:08:57 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,18 @@
+
+radius=float(input("Enter the radius of the circle:"))
+area_of_circle=3.14*radius**2
+print("The area of the circle is",area_of_circle,"square units")
+
+length=float(input("Enter the LENGTH of the Rectangle:"))
+breadth=float(input("Enter the BREADTH of the Rectangle:"))
+area_of_rectangle=length*breadth
+print("Area of the Rectangle is :",area_of_rectangle,"square units")       
+
+side=float(input("Enter the side of the Square:"))
+area_of_square=side**2
+print("The area of the square is :",area_of_square,"square units")
+
+height=float(input("Enter the height of the triangle :"))
+base=float(input("Enter the base of the triangle :"))
+area_of_triangle=0.5*base*height
+print("The area of the Triangle is :",area_of_triangle,"square units")
