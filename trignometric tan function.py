a12b3c7509c92d81d3430bebf0669fe0782331b8
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-03 16:18:28 +0530

    Add files via upload

---
+++ trignometric tan function.py
@@ -0,0 +1,6 @@
+import math
+print("tan(0)=",math.tan(0))
+print("tan(pi/6)=",math.tan(math.pi/6))
+print("tan(pi/4)=",math.tan(math.pi/4))
+print("tan(pi/3)=",math.tan(math.pi/3))
+print("tan(pi/2)=",math.tan(math.pi/2))
