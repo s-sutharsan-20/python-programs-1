d1849aec4601fdb80825c2913b9e96bb5a746446
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-03 16:18:01 +0530

    Add files via upload

---
+++ trignometric sin function.py
@@ -0,0 +1,6 @@
+import math
+print("sin(0)=",math.sin(0))
+print("sin(pi/6)=",math.sin(math.pi/6))
+print("sin(pi/4)=",math.sin(math.pi/4))
+print("sin(pi/3)=",math.sin(math.pi/3))
+print("sin(pi/2)=",math.sin(math.pi/2))
