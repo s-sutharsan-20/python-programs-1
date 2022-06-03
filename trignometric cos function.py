b816664f80a59280ee76b7a3acb10c022afdd61a
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-03 16:17:41 +0530

    Add files via upload

---
+++ trignometric cos function.py
@@ -0,0 +1,6 @@
+import math
+print("cos(0)=",math.cos(0))
+print("cos(pi/6)=",math.cos(math.pi/6))
+print("cos(pi/4)=",math.cos(math.pi/4))
+print("cos(pi/3)=",math.cos(math.pi/3))
+print("cos(pi/2)=",math.cos(math.pi/2))
